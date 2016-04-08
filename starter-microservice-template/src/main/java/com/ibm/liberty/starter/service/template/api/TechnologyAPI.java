082bc5fd3fa2411c63322a383802bea4fad876dd
Kate Stanley <katheris@uk.ibm.com>
2016-04-08 17:30:10 +0100

    Replace 'test' with 'template' in package of template test file. Remove ibm-web-ext.xml file from template as it is not needed.

--- starter-microservice-template/src/main/java/com/ibm/liberty/starter/service/template/api/TechnologyAPI.java
+++ starter-microservice-template/src/main/java/com/ibm/liberty/starter/service/template/api/TechnologyAPI.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package com.ibm.liberty.starter.service.test.api;
+package com.ibm.liberty.starter.service.template.api;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
