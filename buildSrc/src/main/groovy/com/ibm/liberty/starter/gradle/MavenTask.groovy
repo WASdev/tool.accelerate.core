a8d9f437cd06a32ed2eaae58d7f8a50c274083fe
Kate Stanley <katheris@uk.ibm.com>
2016-05-19 11:56:05 +0100

    Update groovy task to take in an array of versions so we still expose earlier versions of the poms. Leave out version 0.0.1 of websocket because it was broken.

--- buildSrc/src/main/groovy/com/ibm/liberty/starter/gradle/MavenTask.groovy
+++ buildSrc/src/main/groovy/com/ibm/liberty/starter/gradle/MavenTask.groovy
@@ -21,13 +21,13 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class MavenTask extends DefaultTask {
-	String id = 'unknown'
-	String version = '0.0.1'
-	boolean hasProvided = false;
-	boolean hasRuntime = false;
-	boolean hasCompile = false;
-	boolean hasServerSnippet = false;
-	
+    String id = 'unknown'
+    String[] pomVersion = ['0.0.1']
+    boolean hasProvided = false;
+    boolean hasRuntime = false;
+    boolean hasCompile = false;
+    boolean hasServerSnippet = false;
+
     MavenTask() {
         setupUpToDateCheckDirectories()
     }
@@ -37,66 +37,69 @@ class MavenTask extends DefaultTask {
         outputs.dir(new File(project.buildDir, 'mavenRepository/artifacts'))
     }
 
-	def getCommandLine() {
-		def os = System.getProperty("os.name").toLowerCase()
-		def mvnCommand = System.getProperty("mvnCommand", "mvn")
-		def osSpecificCommandLine = null
-		if (os.contains("windows")) {
-    		osSpecificCommandLine = ['cmd', '/c', mvnCommand]
-		} else {
-    		osSpecificCommandLine = mvnCommand
-		}
-		return osSpecificCommandLine
-	}
-	
-	def generateMavenInstallArgs = { fileName, artifactId, packaging ->
+    def getCommandLine() {
+        def os = System.getProperty("os.name").toLowerCase()
+        def mvnCommand = System.getProperty("mvnCommand", "mvn")
+        def osSpecificCommandLine = null
+        if (os.contains("windows")) {
+            osSpecificCommandLine = ['cmd', '/c', mvnCommand]
+        } else {
+            osSpecificCommandLine = mvnCommand
+        }
+        return osSpecificCommandLine
+    }
+
+    def generateMavenInstallArgs = { fileName, artifactId, packaging, pomVersion ->
     [ 'install:install-file', 
-      "-Dfile=" + project.projectDir.getAbsolutePath() + "/repository/" + version + "/" + fileName, 
+      "-Dfile=" + project.projectDir.getAbsolutePath() + "/repository/" + pomVersion + "/" + fileName, 
       '-DgroupId=net.wasdev.wlp.starters.' + id, 
       '-DartifactId=' + artifactId, 
-      '-Dversion=' + version, 
+      '-Dversion=' + pomVersion, 
       '-Dpackaging=' + packaging, 
       "-DlocalRepositoryPath=" + project.buildDir.getAbsolutePath() + "/mavenRepository/artifacts", 
       '-DcreateChecksum=true']
-	}
-	
-	@TaskAction
-	def installPOM() {
-		def cmd = getCommandLine()
-		def provargs = generateMavenInstallArgs('provided-pom.xml', 'provided-pom', 'pom')
-		def runargs = generateMavenInstallArgs('runtime-pom.xml', 'runtime-pom', 'pom')
-		def comargs = generateMavenInstallArgs('compile-pom.xml', 'compile-pom', 'pom')
-		def serverargs = generateMavenInstallArgs('server-snippet.xml', 'server-snippet', 'xml')
-		
-		if(hasProvided) {
-			println "Installing provided POM"
-			project.exec {	
-				commandLine cmd
-    			args provargs
-			}
-		}
-		
-		if(hasRuntime) {
-			println "Installing runtime POM"
-			project.exec {	
-				commandLine cmd
-    			args runargs
-			}
-		}
-		if(hasCompile) {
-            println "Installing compile POM"
-            project.exec {  
-                commandLine cmd
-                args comargs
+    }
+
+    @TaskAction
+    def installPOM() {
+        def cmd = getCommandLine()
+        int length = pomVersion.length
+        for (def int i = 0; i < length; i++) {
+            def provargs = generateMavenInstallArgs('provided-pom.xml', 'provided-pom', 'pom', pomVersion[i])
+            def runargs = generateMavenInstallArgs('runtime-pom.xml', 'runtime-pom', 'pom', pomVersion[i])
+            def comargs = generateMavenInstallArgs('compile-pom.xml', 'compile-pom', 'pom', pomVersion[i])
+            def serverargs = generateMavenInstallArgs('server-snippet.xml', 'server-snippet', 'xml', pomVersion[i])
+
+            if(hasProvided) {
+                println "Installing provided POM"
+                project.exec {	
+                    commandLine cmd
+                    args provargs
+                }
+            }
+
+            if(hasRuntime) {
+                println "Installing runtime POM"
+                project.exec {	
+                    commandLine cmd
+                    args runargs
+                }
+            }
+            if(hasCompile) {
+                println "Installing compile POM"
+                project.exec {  
+                    commandLine cmd
+                    args comargs
+                }
+            }
+            if(hasServerSnippet) {
+                println "Installing server snippet"
+                project.exec {  
+                    commandLine cmd
+                    args serverargs
+                }
             }
         }
-        if(hasServerSnippet) {
-            println "Installing server snippet"
-            project.exec {  
-                commandLine cmd
-                args serverargs
-            }
-        }
-	}
-	
+    }
+
 }
