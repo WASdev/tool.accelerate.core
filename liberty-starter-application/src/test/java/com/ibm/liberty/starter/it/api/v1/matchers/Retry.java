6c889afd1dfc1802995f748d34d7f645158b0493
Iain Duncan <iain.duncan@uk.ibm.com>
2016-06-03 08:49:08 +0100

    Update test to handle the fact that the server now runs
    As the server now runs rather than starts the mvn process does not exit
    so need to do it on a separate thread to allow it to do it's thing.
    Also, to make the test dependencies cleaner have switched to using Maven
    embedded so that this will run on a system without Maven already
    installed which also made this platform independent and removed the need
    for a Maven check at the start.
    
    In order to check that the build had completed OK and the server was
    runner we needed more log checking so wrote some Hamcrest matchers to
    keep the tests clean.

---
+++ liberty-starter-application/src/test/java/com/ibm/liberty/starter/it/api/v1/matchers/Retry.java
@@ -0,0 +1,43 @@
+package com.ibm.liberty.starter.it.api.v1.matchers;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+
+/**
+ * Matcher that retries a delegate matcher every 10 seconds for 3 minutes until it matches.
+ */
+public class Retry<T> extends BaseMatcher<T> {
+    
+    private Matcher<T> matcher;
+
+    public Retry(Matcher<T> matcher) {
+        this.matcher = matcher;
+    }
+
+    @Override
+    public boolean matches(Object item) {
+        int attempts = 0;
+        boolean matched = matcher.matches(item);
+        while (!matched && attempts < 18) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                return matched;
+            }
+            matched = matcher.matches(item);
+            attempts++;
+        }
+        return matched;
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("after waiting up to 3 minutes ");
+        description.appendDescriptionOf(matcher);
+    }
+    
+    public static <E> Matcher<E> eventually(Matcher<E> matcher) {
+        return new Retry<E>(matcher);
+    }
+}
