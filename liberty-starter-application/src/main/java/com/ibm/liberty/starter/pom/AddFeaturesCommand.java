9ae1eda3e77910dac69383c066d87aa2658e59be
Kate Stanley <katheris@uk.ibm.com>
2016-12-05 16:51:12 +0000

    Update Swagger to always set acceptLicense to true now that we have accept and download button. Also update swagger tech type to enable servlet-3.1 rather than servlet-3.0 by default.
    Signed-off-by: Kate Stanley <katheris@uk.ibm.com>

--- liberty-starter-application/src/main/java/com/ibm/liberty/starter/pom/AddFeaturesCommand.java
+++ liberty-starter-application/src/main/java/com/ibm/liberty/starter/pom/AddFeaturesCommand.java
@@ -43,8 +43,7 @@ public class AddFeaturesCommand implements PomModifierCommand {
             addInstallFeatureExecution(doc, configuration.getParentNode());
 
             if (!DomUtil.hasChildNode(features, "acceptLicense")) {
-                DomUtil.addChildNode(doc, features, "acceptLicense", "${accept.features.license}");
-                enforceAcceptLicenseProperty(doc, configuration.getParentNode().getParentNode());
+                DomUtil.addChildNode(doc, features, "acceptLicense", "true");
             }
 
             //Add the features
@@ -66,51 +65,6 @@ public class AddFeaturesCommand implements PomModifierCommand {
     private Node getLibertyMavenPluginConfiguration(Document doc) {
         Node assemblyInstallDirectory = doc.getElementsByTagName("assemblyInstallDirectory").item(0);
         return assemblyInstallDirectory.getParentNode();
-    }
-
-    private void enforceAcceptLicenseProperty(Document doc, Node pluginsNode) {
-        //Enforce 'accept.features.license' property using maven-enforcer-plugin
-        Node enforcerPlugin;
-        Node artifactIdNode = DomUtil.getGrandchildNode(pluginsNode, "plugin", "artifactId", "maven-enforcer-plugin");
-        if (artifactIdNode == null) {
-            enforcerPlugin = DomUtil.addChildNode(doc, pluginsNode, "plugin", null);
-            DomUtil.addChildNode(doc, enforcerPlugin, "groupId", "org.apache.maven.plugins");
-            DomUtil.addChildNode(doc, enforcerPlugin, "artifactId", "maven-enforcer-plugin");
-            DomUtil.addChildNode(doc, enforcerPlugin, "version", "1.4.1");
-        } else {
-            enforcerPlugin = artifactIdNode.getParentNode();
-        }
-
-        Node executions = DomUtil.findOrAddChildNode(doc, enforcerPlugin, "executions", null);
-
-        Node execution;
-        Node enforceProperty = DomUtil.getGrandchildNode(executions, "execution", "id", "enforce-property");
-        if (enforceProperty == null) {
-            execution = DomUtil.addChildNode(doc, executions, "execution", null);
-            DomUtil.addChildNode(doc, execution, "id", "enforce-property");
-        } else {
-            execution = enforceProperty.getParentNode();
-        }
-
-        DomUtil.findOrAddChildNode(doc, execution, "phase", "validate");
-
-        Node goals = DomUtil.findOrAddChildNode(doc, execution, "goals", null);
-        DomUtil.findOrAddChildNode(doc, goals, "goal", "enforce");
-
-        Node configurationNode = DomUtil.findOrAddChildNode(doc, execution, "configuration", null);
-        Node rules = DomUtil.findOrAddChildNode(doc, configurationNode, "rules", null);
-        Node propertyNode = DomUtil.getGrandchildNode(rules, "requireProperty", "property", "accept.features.license");
-        if (propertyNode == null) {
-            Node requireProperty = DomUtil.addChildNode(doc, rules, "requireProperty", null);
-            DomUtil.addChildNode(doc, requireProperty, "property", "accept.features.license");
-            DomUtil.addChildNode(doc, requireProperty, "message",
-                                 "You must set a value for the 'accept.features.license' property defined in pom.xml. Please review the license terms and conditions for additional features to be installed and if you accept the license terms and conditions then run the Maven command with '-Daccept.features.license=true'.");
-            DomUtil.addChildNode(doc, requireProperty, "regex", "true");
-            DomUtil.addChildNode(doc, requireProperty, "regexMessage",
-                                 "Additional features could not be installed as the license terms and conditions were not accepted. If you accept the license terms and conditions then run the Maven command with '-Daccept.features.license=true'.");
-        }
-
-        DomUtil.findOrAddChildNode(doc, configurationNode, "fail", "true");
     }
 
 }
