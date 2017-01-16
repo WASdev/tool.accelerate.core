ade2afe61aed8766877e5c882292e208fac10a8d
MarkNSweep <MarkNSweep@github.com>
2017-01-16 14:52:45 +0000

    Update to use response object for setting headers Signed off by : Adam Pilkington apilkington@uk.ibm.com

--- liberty-starter-application/src/main/java/com/ibm/liberty/starter/api/CORSFilter.java
+++ liberty-starter-application/src/main/java/com/ibm/liberty/starter/api/CORSFilter.java
@@ -21,12 +21,14 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class CORSFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext req, ContainerResponseContext res) throws IOException {
-        MultivaluedMap<String, String> headers = req.getHeaders();
+        MultivaluedMap<String, Object> headers = res.getHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
         headers.add("Access-Control-Allow-Methods", "GET, POST");
         headers.add("Access-Control-Max-Age", "86400");
