71ca65f32884c5ed4b7dcf3d33692ee6fbb288db
Kate Stanley <katheris@uk.ibm.com>
2017-03-16 12:50:38 +0000

    Add extra tests.
    Signed-off-by: Kate Stanley <katheris@uk.ibm.com>

--- liberty-starter-application/src/test/java/com/ibm/liberty/starter/availability/EndpointAvailabilityTest.java
+++ liberty-starter-application/src/test/java/com/ibm/liberty/starter/availability/EndpointAvailabilityTest.java
@@ -12,22 +12,50 @@ import org.junit.Test;
 public class EndpointAvailabilityTest {
 	
     private String port = System.getProperty("liberty.test.port");
-    private String url = "http://localhost:" + port + "/start/";
 
     @Test
-    public void testEndpoint() throws Exception {
+    public void testLocalHostEndpoint() throws Exception {
+        String url = "http://localhost:" + port + "/";
         System.out.println("Testing endpoint " + url);
         int maxCount = 30;
-        int responseCode = makeRequest();
+        int responseCode = makeRequest(url);
         for(int i = 0; (responseCode != 200) && (i < maxCount); i++) {
           System.out.println("Response code : " + responseCode + ", retrying ... (" + i + " of " + maxCount + ")");
           Thread.sleep(5000);
-          responseCode = makeRequest();
+          responseCode = makeRequest(url);
+        }
+        assertTrue("Incorrect response code: " + responseCode, responseCode == 200);
+    }
+    
+    @Test
+    public void testEndpoint() throws Exception {
+    	String url = "http://172.0.0.1:" + port + "/";
+        System.out.println("Testing endpoint " + url);
+        int maxCount = 30;
+        int responseCode = makeRequest(url);
+        for(int i = 0; (responseCode != 200) && (i < maxCount); i++) {
+          System.out.println("Response code : " + responseCode + ", retrying ... (" + i + " of " + maxCount + ")");
+          Thread.sleep(5000);
+          responseCode = makeRequest(url);
+        }
+        assertTrue("Incorrect response code: " + responseCode, responseCode == 200);
+    }
+    
+    @Test
+    public void testStartEndpoint() throws Exception {
+        String url = "http://localhost:" + port + "/start/";
+        System.out.println("Testing endpoint " + url);
+        int maxCount = 30;
+        int responseCode = makeRequest(url);
+        for(int i = 0; (responseCode != 200) && (i < maxCount); i++) {
+          System.out.println("Response code : " + responseCode + ", retrying ... (" + i + " of " + maxCount + ")");
+          Thread.sleep(5000);
+          responseCode = makeRequest(url);
         }
         assertTrue("Incorrect response code: " + responseCode, responseCode == 200);
     }
 
-    private int makeRequest() {
+    private int makeRequest(String url) {
       Client client = ClientBuilder.newClient();
       Invocation.Builder invoBuild = client.target(url).request();
       Response response = invoBuild.get();
